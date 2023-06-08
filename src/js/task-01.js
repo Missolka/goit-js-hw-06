44d1917e164d10af29bb8450628014ca1395ece4
Missolka <olchik.burmaka@gmail.com>
2023-06-08 14:10:04 +0300

    added changes

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,11 @@
-const categoriesList = document.querySelector('#categories');
+const categoryList = document.querySelector('#categories');
 console.log(`Number of categories: ${categoriesList.children.length}`);
 categoriesList.querySelectorAll('li.item').forEach(category => {
   });
   const title = category.querySelector('h2').textContent;
   const elementsCount = category.querySelectorAll('li').length;
   console.log(`Category: ${title}\nElements: ${elementsCount}`);
-  const categoriesList = document.querySelector('#categories');
+  const categoryist = document.querySelector('#categories');
   console.log(`Number of categories: ${categoriesList.children.length}`);
   categoriesList.querySelectorAll('li.item').forEach(category => {
     const title = category.querySelector('h2').textContent;
